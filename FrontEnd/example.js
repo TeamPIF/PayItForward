efa76ab325374aafafd50aa4054f1145c8958233
Evan King <esk79@cornell.edu>
2015-11-14 16:11:25 -0500

    here bitch

---
+++ FrontEnd/example.js
@@ -0,0 +1,4 @@
+/**
+ * Created by EvanKing on 11/14/15.
+ */
+console.log("Hello World");
--- FrontEnd/hello.js
+++ FrontEnd/hello.js
@@ -1,9 +1,18 @@
 
+
+//var Request = unirest.get('http://localhost:4567/hello');
+//document.getElementById("hello").innerHTML = Request.toString();
+
+
+//var carName = "Volvo";
+//document.getElementById("demo").innerHTML = carName;
+
+
 var unirest = require('unirest');
 
-var Request = unirest.get('http://localhost:4567/hello');
-document.getElementById("hello").innerHTML = Request.toString();
-console.log(Request.toString());
+unirest.get('http://localhost:4567/hello')
+    .send()
+    .end(function (response) {
+        document.getElementById("demo").innerHTML = response.body;
+    });
 
-var carName = "Volvo";
-document.getElementById("demo").innerHTML = carName;
