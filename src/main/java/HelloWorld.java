811f3ff7b15cd396d669b31cdc16f474b5dd64c4
Evan King <esk79@cornell.edu>
2015-11-14 14:11:18 -0500

    here we go

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by EvanKing on 11/14/15.
+ */
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
