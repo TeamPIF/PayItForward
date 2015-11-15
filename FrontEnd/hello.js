3a11b7a1895e314da9e7425eea5e9eaf746d80c3
noahrubin <noahski@benrubin.net>
2015-11-14 19:52:06 -0500

    fixed CORS issue

--- FrontEnd/hello.js
+++ FrontEnd/hello.js
@@ -1,18 +1,24 @@
 
 
-//var Request = unirest.get('http://localhost:4567/hello');
-//document.getElementById("hello").innerHTML = Request.toString();
 
 
-//var carName = "Volvo";
-//document.getElementById("demo").innerHTML = carName;
 
 
-var unirest = require('unirest');
 
-unirest.get('http://localhost:4567/hello')
-    .send()
-    .end(function (response) {
-        document.getElementById("demo").innerHTML = response.body;
-    });
-
+// var unirest = require('unirest');
+//
+// unirest.get('http://localhost:4567/hello')
+//     .send()
+//     .end(function (response) {
+//         document.getElementById("demo").innerHTML = response.body;
+//     });
+//
+//
+//
+//
+// //var Request = unirest.get('http://localhost:4567/hello');
+// //document.getElementById("hello").innerHTML = Request.toString();
+//
+//
+// //var carName = "Volvo";
+// //document.getElementById("demo").innerHTML = carName;
