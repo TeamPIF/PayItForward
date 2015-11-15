a73fa79951f57b6ffb70e222b079518aad903c91
noahrubin <noahski@benrubin.net>
2015-11-15 01:51:28 -0500

    added values in database

--- InitMVP.sql
+++ InitMVP.sql
@@ -2,10 +2,21 @@
 -- 
 -- USE ffcMVP;
 
+CREATE TABLE Business (
+	id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE Thank_You (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255),
 	message TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
 
@@ -13,7 +24,8 @@ CREATE TABLE Donation (
 	id INT NOT NULL AUTO_INCREMENT,
 	business_id INT,	
 	doner_id INT,
-	donation_time TIMESTAMP,
+	donation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (business_id) REFERENCES Business(id)
 );
@@ -21,22 +33,18 @@ CREATE TABLE Donation (
 CREATE TABLE Claim (
 	id INT NOT NULL AUTO_INCREMENT,
     business_id INT NOT NULL,
-    claim_time TIMESTAMP,
+    claim_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (business_id) REFERENCES Business(id)
-);
-
-CREATE TABLE Business (
-	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    address TEXT,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE Credentials (
     business_id INT NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (business_id) REFERENCES Business(id)
 );
 
@@ -47,7 +55,20 @@ INSERT INTO Thank_You (id, name, message) VALUES
     (4, "Baird Howland", "We're gonna win RoHK"),
     (5, "Ishaan Javheri", "We're gonna make those homeless people happier than anyone else in this building could, Bonobos style.");
 
+INSERT INTO Business (id, name, address) VALUES 
+	(1, "Dick's Barn Yard", "115 Suck Me Avenue Ithaca, NY 14850"),
+    (2, "Model's", "26 Ridge Road Ithaca, NY 14850"),
+    (3, "La Croix", "3 Tuxedo Drive Ithaca, NY 14850");
 
+INSERT INTO Donation (id, business_id, doner_id) VALUES 
+	(1, 1, NULL),
+    (2, 1, NULL),
+    (3, 2, NULL),
+    (4, 2, NULL),
+    (5, 3, NULL);
 
--- DROP TABLE Thank_You;-- 
-
+DROP TABLE IF EXISTS Thank_You;
+DROP TABLE IF EXISTS Claim;
+DROP TABLE IF EXISTS Business;
+DROP TABLE IF EXISTS Donation;
+DROP TABLE IF EXISTS Credentials;
