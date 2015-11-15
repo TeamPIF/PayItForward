05feb24ccbd5a94b8f1b426319078291af5cf6bf
noahrubin <noahski@benrubin.net>
2015-11-15 11:03:40 -0500

    fixed API calls

--- PIF_FrontEnd/js/donate.js
+++ PIF_FrontEnd/js/donate.js
@@ -3,19 +3,11 @@ domain = "http://127.0.0.1:4567";
 $.when(
     $.get(domain + "/homepage/donated"),
     $.get(domain + "/homepage/served"),
-    $.get(domain + "/homepage/available_all"),
-    $.get(domain + "/homepage/num_partners")
+    $.get(domain + "/homepage/available_all")
+    // $.get(domain + "/homepage/num_partners")
 ).done(function(donated, served, avail, partners){
     $("#mealsDonated").html(Number(JSON.parse(donated[0]).sum));
     $("#mealsServed").html(Number(JSON.parse(served[0]).sum));
-    $("#localPartners").html(Number(JSON.parse(avail[0]).sum));
-    $("#sinceLaunch").html(Number(JSON.parse(partners[0]).sum));
+    $("#available").html(Number(JSON.parse(avail[0]).sum));
+    // $("#localPartners").html(Number(JSON.parse(partners[0]).sum));
 });
-//
-// $.post("http://localhost:4567/stories/submit",
-//     JSON.stringify(
-//     {
-//         name: "Ishaan",
-//         message: "I love this thing!!"
-//     })
-// );
