94d58222f284d91ec84cefcf3f51585e9ebd18bf
noahrubin <noahski@benrubin.net>
2015-11-15 05:43:06 -0500

    added POST request to donate.js

--- PIF_FrontEnd/js/donate.js
+++ PIF_FrontEnd/js/donate.js
@@ -11,3 +11,11 @@ $.when(
     $("#localPartners").html(Number(JSON.parse(avail[0]).sum));
     $("#sinceLaunch").html(Number(JSON.parse(partners[0]).sum));
 });
+
+$.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:4567/stories/submit",
+    data: {"name": "Horris Grant",
+            "message" : "You guys saved me, I used to make millions in the NBA now I'm broke"
+          }
+}).done(function(data) {console.log(data);});
